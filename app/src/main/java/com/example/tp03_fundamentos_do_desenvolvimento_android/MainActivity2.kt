1e7f536d031ce74aa4aa97b9baf3cac01c5af947
ThiagoSegadaes <Thiagopace2002@gmail.com>
2022-06-06 20:43:41 -0300

    first commit

---
+++ app/src/main/java/com/example/tp03_fundamentos_do_desenvolvimento_android/MainActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.tp03_fundamentos_do_desenvolvimento_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
